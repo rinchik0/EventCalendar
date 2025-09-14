56bfd0b51d3fcbada03c7b37d9b07754bfcbe9f8
rinchik0 <ariadna.kolupaeva@yandex.ru>
2025-09-14 17:29:51 +0300

    Update some files

--- src/main/java/com/rinchik/practise/controller/UserController.java
+++ src/main/java/com/rinchik/practise/controller/UserController.java
@@ -11,8 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.security.Principal;
-
 @Controller
 @RequestMapping("/users")
 @RequiredArgsConstructor
